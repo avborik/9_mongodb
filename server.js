9bfb64df69b19dba3a633b5ee40e237237d02d06
AvBorikDeveloper <avboris28@gmail.com>
2019-05-25 00:13:41 +1200

    mongoose, deleting data

--- server.js
+++ server.js
@@ -18,14 +18,54 @@ const Car = mongoose.model('Car',carSchema);
 //     console.log(doc)
 // })
 
-const addCar = new Car({
-    brand: 'Ford',
-    model: 'Focus',
-    year: '2017',
-    avail: true
-})
+// 
 
-addCar.save((err,doc)=>{
+// Car.find({brand:"Ford"},(err,doc)=>{
+//     if(err) return console.log(err);
+//     console.log(doc)
+// })
+
+// Car.find({_id:""},(err,doc)=>{
+//     if(err) return console.log(err);
+//     console.log(doc)
+// })
+
+// Car.findByIdAndRemove({brand:'Ford'},(err,doc)=>{
+//     if(err) return console.log(err);
+//     console.log(doc)
+// })
+
+// Car.remove({year:2000},(err,doc)=>{
+//     if(err) return console.log(err);
+//     console.log(doc)
+// })
+
+// Car.update(
+//     {_id:""},
+//     {$set:{brand:"Nissan"}},
+//     (err,doc)=>{
+//         if(err) return console.log(err);
+//         console.log(doc)
+//     }
+// )
+
+// Car.findByIdAndUpdate("",
+//     {$set:{brand:""}},
+//     {new: false},
+//     (err,doc)=>{
+//         if(err) return console.log(err);
+//         console.log(doc)
+//     }
+// )
+
+Car.findById("",(err,car)=>{
     if(err) return console.log(err);
-    console.log(doc)
+
+    car.set({
+        brand: "Porche"
+    });
+    car.save((err,doc)=>{
+        if(err) return console.log(err);
+        console.log(doc)
+    })
 })
